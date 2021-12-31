dca44843ac6345171d0f79afbf00d50aadf1ae16
mandlg <gabriel.mandli@msg.group>
2021-12-31 10:40:38 +0100

    IG

--- src/test/java/com/mandli/diplomovka/DiplomovkaApplicationTests.java
+++ src/test/java/com/mandli/diplomovka/DiplomovkaApplicationTests.java
@@ -2,12 +2,31 @@ package com.mandli.diplomovka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class DiplomovkaApplicationTests {
+
+    @Container
+    public static MySQLContainer container = new MySQLContainer("mysql:8")
+            .withUsername("testContainersDbUser")
+            .withPassword("testContainersDbPassword")
+            .withDatabaseName("testContainersDb");
+    
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
 
     @Test
     void contextLoads() {
     }
-
 }
